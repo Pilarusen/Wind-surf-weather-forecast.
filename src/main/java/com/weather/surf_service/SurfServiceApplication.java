e5a64f0a2ce1d05a90db130f6959d5986703e421
kamilles23 <k.kamilles23@gmail.com>
2022-11-21 18:08:30 +0100

    initial commit
    (cherry picked from commit 232bafaafa3efb1c715a23d57ba041529f6c29c2)

---
+++ src/main/java/com/weather/surf_service/SurfServiceApplication.java
@@ -0,0 +1,13 @@
+package com.weather.surf_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurfServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurfServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weather/surf_service/SurfServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather.surf_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurfServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
