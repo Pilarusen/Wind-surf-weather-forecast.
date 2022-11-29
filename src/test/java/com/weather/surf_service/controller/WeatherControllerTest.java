eb4730d6472fce6ad7fa53b15bfbdf461a7af307
kamilles23 <k.kamilles23@gmail.com>
2022-11-29 14:16:42 +0100

    Integration test WeatherController

--- src/test/java/com/weather/surf_service/controller/WeatherControllerTest.java
+++ src/test/java/com/weather/surf_service/controller/WeatherControllerTest.java
@@ -31,7 +31,7 @@ class WeatherControllerTest {
     WeatherClient weatherClient;
 
     @Test
-    public void shouldReturnStatus2xx() throws Exception {
+    public void allParametersOKShouldReturnStatus2xx() throws Exception {
         //given
         String locationDate = String.valueOf(LocalDate.now().plusDays(2));
         String locationName = "JASTARNIA_POLAND";
@@ -46,4 +46,91 @@ class WeatherControllerTest {
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
     }
+
+    @Test
+    public void allParametersOKNoneOfLocationsMeetsRequirementsShouldReturnStatus204() throws Exception {
+        //given
+        String locationDate = String.valueOf(LocalDate.now().plusDays(2));
+        String locationName = "JASTARNIA_POLAND";
+        Map<String, String> coordinates = new HashMap<>(Collections.singletonMap("54.70", "18.67"));
+        var locationList = List.of(new LocationDTO(locationName, locationDate, "0", "0"));
+        Forecast mockForecast = new Forecast(null, locationList);
+        String url = "/best-weather/date=" + locationDate;
+        //when
+        when(weatherClient.getWeatherForCityCoOrdinates(locationName, coordinates, 2)).thenReturn(mockForecast);
+        //then
+        mockMvc.perform(get(url))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    public void dateFromPastShouldReturnStatus400() throws Exception {
+        //given
+        String locationDate = String.valueOf(LocalDate.now().minusDays(2));
+        String locationName = "JASTARNIA_POLAND";
+        Map<String, String> coordinates = new HashMap<>(Collections.singletonMap("54.70", "18.67"));
+        var locationList = List.of(new LocationDTO(locationName, locationDate, "6", "6"));
+        Forecast mockForecast = new Forecast(null, locationList);
+        String url = "/best-weather/date=" + locationDate;
+        //when
+        when(weatherClient.getWeatherForCityCoOrdinates(locationName, coordinates, 2)).thenReturn(mockForecast);
+        //then
+        mockMvc.perform(get(url))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    public void dateToFarAwayShouldReturnStatus400() throws Exception {
+        //given
+        String locationDate = String.valueOf(LocalDate.now().plusDays(17));
+        String locationName = "JASTARNIA_POLAND";
+        Map<String, String> coordinates = new HashMap<>(Collections.singletonMap("54.70", "18.67"));
+        var locationList = List.of(new LocationDTO(locationName, locationDate, "6", "6"));
+        Forecast mockForecast = new Forecast(null, locationList);
+        String url = "/best-weather/date=" + locationDate;
+        //when
+        when(weatherClient.getWeatherForCityCoOrdinates(locationName, coordinates, 2)).thenReturn(mockForecast);
+        //then
+        mockMvc.perform(get(url))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    public void dateFormatIncorrectShouldReturnStatus400() throws Exception {
+        //given
+        String locationDate = "Incorrect date format.";
+        String locationName = "JASTARNIA_POLAND";
+        Map<String, String> coordinates = new HashMap<>(Collections.singletonMap("54.70", "18.67"));
+        var locationList = List.of(new LocationDTO(locationName, locationDate, "6", "6"));
+        Forecast mockForecast = new Forecast(null, locationList);
+        String url = "/best-weather/date=" + locationDate;
+        //when
+        when(weatherClient.getWeatherForCityCoOrdinates(locationName, coordinates, 2)).thenReturn(mockForecast);
+        //then
+        mockMvc.perform(get(url))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    //TODO can not test that because of early checking in class WeatherClient
+//    @Test
+//    public void wrongLocationCoordinatesShouldReturnStatus500() throws Exception {
+//        //given
+//        String locationDate = String.valueOf(LocalDate.now().plusDays(2));
+//        String locationName = "JASTARNIA_POLAND";
+//        Map<String, String> coordinates = new HashMap<>(Collections.singletonMap("wrong", "wrong"));
+//        var locationList = List.of(new LocationDTO(locationName, locationDate, "6", "6"));
+//        Forecast mockForecast = new Forecast(null, locationList);
+//        String url = "/best-weather/date=" + locationDate;
+//        //when
+//        //then
+//        mockMvc.perform(get(url))
+//                .andDo(MockMvcResultHandlers.print())
+//                .andExpect(MockMvcResultMatchers.status().isInternalServerError());
+//    }
+
+    //TODO add test when api not working
 }
